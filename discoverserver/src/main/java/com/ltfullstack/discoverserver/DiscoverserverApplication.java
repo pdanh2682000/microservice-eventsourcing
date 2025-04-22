719a68231e799b0006604839c41252272c5ecd84
Phan Duy Anh <duy-anhp@system-exe.com.vn>
2025-04-22 16:33:20 +0700

    update controller

--- discoverserver/src/main/java/com/ltfullstack/discoverserver/DiscoverserverApplication.java
+++ discoverserver/src/main/java/com/ltfullstack/discoverserver/DiscoverserverApplication.java
@@ -3,6 +3,8 @@ package com.ltfullstack.discoverserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @EnableEurekaServer
 @SpringBootApplication
