be12f94b195c6fceee9402293430623f018addb9
Phan Duy Anh <duy-anhp@system-exe.com.vn>
2025-04-22 16:28:14 +0700

    update controller

--- discoverserver/src/main/java/com/ltfullstack/discoverserver/DiscoverserverApplication.java
+++ discoverserver/src/main/java/com/ltfullstack/discoverserver/DiscoverserverApplication.java
@@ -7,8 +7,11 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 @SpringBootApplication
 public class DiscoverserverApplication {
+	
+	private static final Logger logger = LoggerFactory.getLogger(DiscoverserverApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("-------------------------- DA TEST CI/CD -----------------------");
 		SpringApplication.run(DiscoverserverApplication.class, args);
 	}
 
